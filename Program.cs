d71c87aab15f1ef3e5ec099810b74af3d49ff5b2
Mariann <bogdan.mariann@petrik.hu>
2026-02-23 15:05:45 +0100

    hutos feladat teljesen kesz

--- Program.cs
+++ Program.cs
@@ -1,10 +1,108 @@
-﻿namespace Huto
+﻿using System.IO;
+
+namespace Huto
 {
 	internal class Program
 	{
+		static List<string> huto = new List<string>();
+
+		static void Listazas()
+		{
+		
+			Console.WriteLine("A hűtő tartalma:");
+			foreach (var item in huto)
+			{
+				Console.WriteLine(item.ToLower());
+			}
+		}
+		static void EtelHutobeRakasa()
+		{
+			string valasz;
+			do
+			{
+				Console.WriteLine("Milyen ételt szeretnél berakni a hűtőbe?");
+				valasz = Console.ReadLine();
+				if (valasz != "")
+				{
+					huto.Add(valasz);
+				}
+			}
+			while (valasz != ""); //bennmaradas feltétele. Mikor fusson ujra
+
+		}
+		static void FajlbaIras()
+		{
+			StreamWriter sr = new StreamWriter("huto.txt");
+			foreach (var item in huto)
+			{
+				string s = $"{item.ToLower()}\n";
+				sr.Write(s);
+			}
+			sr.Close();
+		}
+
+		static void FajlbeOlvasas()
+		{
+			StreamReader sr = new StreamReader("huto.txt");
+			while (!sr.EndOfStream)
+			{
+				huto.Add(sr.ReadLine());
+			}
+			sr.Close();
+
+		}
+
+		static void EtelKivetel()
+		{
+			Console.WriteLine("Milyen ételt szeretnél kivenni a hűtőből?");
+			string etel = Console.ReadLine();
+			if (huto.Contains(etel.ToLower())) {
+				huto.Remove(etel);
+			} 
+		}
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello, World!");
+			char valasz;
+			do
+			{
+                Console.WriteLine("-----------------------------------------------------------------------");
+                Console.WriteLine("Menu: \n\t1. listázás  \n\t2. Étel berakása a hűtőbe \n\t3. Étel kivétele \n\t4.Fájlbairás\n\t5. FajlBeolvasasa\n\t6 Takarítás\n\t7 kilépés");
+				valasz = Console.ReadLine()[0];
+				switch (valasz)
+				{
+					case '1':
+						Listazas(); 
+						break;
+					case '2':
+						EtelHutobeRakasa();
+						break;
+					case '3':
+					
+						break;
+
+					case '4':
+						FajlbaIras();
+						break;
+					case '5':
+						FajlbeOlvasas();
+						break;
+					case '6':
+						huto.Clear();
+						Console.WriteLine("A hűtő kiürítve!");
+						break;
+
+
+					case '7':
+						Console.WriteLine("Viszlát!");
+						FajlbaIras();
+						break;
+				}
+            }
+			while (valasz != '6');
+
+
+
+
 		}
 	}
 }
